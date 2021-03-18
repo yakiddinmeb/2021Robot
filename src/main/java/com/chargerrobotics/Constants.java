a0afa991f9bf728e63e7456dcc514364118a60d5
jason k <71300950+yakiddinmeb@users.noreply.github.com>
2021-03-17 20:23:07 -0700

    attempt 6

--- src/main/java/com/chargerrobotics/Constants.java
+++ src/main/java/com/chargerrobotics/Constants.java
@@ -8,121 +8,119 @@
 package com.chargerrobotics;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants. This class should not be used for any other
- * purpose. All constants should be declared globally (i.e. public static). Do
- * not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the constants are needed, to reduce verbosity.
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-	// Controller IDs
-	public static final int primary = 0;
-	public static final int secondary = 1;
+  // Controller IDs
+  public static final int primary = 0;
+  public static final int secondary = 1;
 
-	// CAN Id's
-	// 1: Power distribution panel (PDP)
-	// 2: Pneumatic Control Module (PCM)
-	// 1x: Drive: Odds are left, evens are right
-	// 2x: Shooter
-	// 3x: Color Spinner
-	public static final int powerDistributionPanel = 1;
-	public static final int rightFrontDrive = 11;
-	public static final int rightRearDrive = 12;
-	public static final int leftRearDrive = 13;
-	public static final int leftFrontDrive = 14;
-	public static final int shooterLeft = 21;
-	public static final int shooterRight = 22;
-	public static final int shooterHood = 23;
-	public static final int colorSpinner = 31;
-	public static final int chomperFeed = 41;
-	public static final int chomperLift = 42;
-  	public static final int feedStage = 43;
-	public static final int kicker = 44;
-	public static final int climbExtender = 51;
-	public static final int climbPush2 = 52;
-	public static final int climbPull = 53;
+  // CAN Id's
+  // 1: Power distribution panel (PDP)
+  // 2: Pneumatic Control Module (PCM)
+  // 1x: Drive: Odds are left, evens are right
+  // 2x: Shooter
+  // 3x: Color Spinner
+  public static final int powerDistributionPanel = 1;
+  public static final int rightFrontDrive = 11;
+  public static final int rightRearDrive = 12;
+  public static final int leftRearDrive = 13;
+  public static final int leftFrontDrive = 14;
+  public static final int shooterLeft = 21;
+  public static final int shooterRight = 22;
+  public static final int shooterHood = 23;
+  public static final int colorSpinner = 31;
+  public static final int chomperFeed = 41;
+  public static final int chomperLift = 42;
+  public static final int feedStage = 43;
+  public static final int kicker = 44;
+  public static final int climbExtender = 51;
+  public static final int climbPush2 = 52;
+  public static final int climbPull = 53;
 
-	//PWM IDs
-	public static final int colorSpinnerLifter = 9;
-	public static final int leds = 1;
+  // PWM IDs
+  public static final int colorSpinnerLifter = 9;
+  public static final int leds = 1;
 
-	//Digital In/Out Ports
-	public static final int chomperLimitSwitch = 9;
-	public static final int hoodLimitSwitch = 1;
+  // Digital In/Out Ports
+  public static final int chomperLimitSwitch = 9;
+  public static final int hoodLimitSwitch = 1;
 
-	//Chomper Constants
-	public static final int chomperDistToDown = 550;
-	public static final int chomperDistBottomToUp = 1999;
+  // Chomper Constants
+  public static final int chomperDistToDown = 550;
+  public static final int chomperDistBottomToUp = 1999;
 
-	// Shooter Constants
-	public static final double shooterTargetRPM = 2500.0;
-	public static final double shooterP = 0.0005;
-	public static final double shooterI = 0.00000047;
-	public static final double shooterD = 0.014;
-	public static final int shooterCurrentLimit = 40;
-	public static final double shooterFeedForward = 0.0;
-	public static final double shooterStaticGain = 0.0;
-	public static final double shooterVelocityGain = 0.0;
-	public static final double shooterMinOutput = -1.0;
-	public static final double shooterMaxOutput = 1.0;
-	
-	public static final double desiredDistance = 120.0;
+  // Shooter Constants
+  public static final double shooterTargetRPM = 2500.0;
+  public static final double shooterP = 0.0005;
+  public static final double shooterI = 0.00000047;
+  public static final double shooterD = 0.014;
+  public static final int shooterCurrentLimit = 40;
+  public static final double shooterFeedForward = 0.0;
+  public static final double shooterStaticGain = 0.0;
+  public static final double shooterVelocityGain = 0.0;
+  public static final double shooterMinOutput = -1.0;
+  public static final double shooterMaxOutput = 1.0;
 
-	// Shooter Hood Constants
-	public static final int hoodPIDLoopId = 0;
-	public static final int hoodGainSlot = 0;
-	public static final int hoodTimeOutMs = 30;
-	public static final int ticksPerRev = 0;
-	public static final double hoodP = 0.0007;
-	public static final double hoodI = 0.00038;
-	public static final double hoodD = 0.0;
-	public static final double hoodF = 0.0;
+  public static final double desiredDistance = 120.0;
 
-	public static final double hoodPresetAngle = 500;
-	public static final double hoodRetractAngle = 1800;
-	public static final double defaultHoodSpeed = 0.30;
+  // Shooter Hood Constants
+  public static final int hoodPIDLoopId = 0;
+  public static final int hoodGainSlot = 0;
+  public static final int hoodTimeOutMs = 30;
+  public static final int ticksPerRev = 0;
+  public static final double hoodP = 0.0007;
+  public static final double hoodI = 0.00038;
+  public static final double hoodD = 0.0;
+  public static final double hoodF = 0.0;
 
+  public static final double hoodPresetAngle = 500;
+  public static final double hoodRetractAngle = 1800;
+  public static final double defaultHoodSpeed = 0.30;
 
-	// File Names
-	public static final String dataStoragePath = "/home/lvuser";
-	public static final String configFileName = "config.yml";
+  // File Names
+  public static final String dataStoragePath = "/home/lvuser";
+  public static final String configFileName = "config.yml";
 
-	// Limelight distance calculation constants
+  // Limelight distance calculation constants
 
-	// Placeholder X and Y coords of markers on field in inches, given initiation line is marked south and the bottom left corner is (0, 0):
-	public static final double MARKER_ONE_X = 0;
-	public static final double MARKER_ONE_Y = 0;
-	public static final double MARKER_TWO_X = 0;
-	public static final double MARKER_TWO_Y = 21;
-	public static final double MARKER_THREE_X = 60;
-	public static final double MARKER_THREE_Y = 21;
-	public static final double MARKER_FOUR_X = 60;
-	public static final double MARKER_FOUR_Y = 0;
+  // Placeholder X and Y coords of markers on field in inches, given initiation line is marked south
+  // and the bottom left corner is (0, 0):
+  public static final double MARKER_ONE_X = 0;
+  public static final double MARKER_ONE_Y = 0;
+  public static final double MARKER_TWO_X = 0;
+  public static final double MARKER_TWO_Y = 21;
+  public static final double MARKER_THREE_X = 60;
+  public static final double MARKER_THREE_Y = 21;
+  public static final double MARKER_FOUR_X = 60;
+  public static final double MARKER_FOUR_Y = 0;
 
-	// Pipelines corresponding to significant targets:
-	public static final int shooterTarget = 0;
-	public static final int fuelTarget = 1;
-	public static final int MARKER1 = 2;
-	public static final int MARKER2 = 3;
-	public static final int MARKER3 = 4;
-	public static final int MARKER4 = 5;
+  // Pipelines corresponding to significant targets:
+  public static final int shooterTarget = 0;
+  public static final int fuelTarget = 1;
+  public static final int MARKER1 = 2;
+  public static final int MARKER2 = 3;
+  public static final int MARKER3 = 4;
+  public static final int MARKER4 = 5;
 
-	// Blitzen config:
-	//public static final double targetHeight = 94.0; // inches
-	//public static final double cameraHeight = 24.0; // inches
-	//public static final double cameraAngle = 30.0; // degrees
+  // Blitzen config:
+  // public static final double targetHeight = 94.0; // inches
+  // public static final double cameraHeight = 24.0; // inches
+  // public static final double cameraAngle = 30.0; // degrees
 
-	// 2020 bot config:
-	public static final double targetHeight = 91.5; // inches
-	public static final double cameraHeight = 23.75; // inches
-	public static final double cameraAngle = 29.0; // degrees
-	public static final String comPortsFileName = "com.yml";
+  // 2020 bot config:
+  public static final double targetHeight = 91.5; // inches
+  public static final double cameraHeight = 23.75; // inches
+  public static final double cameraAngle = 29.0; // degrees
+  public static final String comPortsFileName = "com.yml";
 
-	/*
-	Tests at Tahoma
-	Front bumper on the line: 3000 RPM, 55 degrees hood, distance 131 inches
-	*/
+  /*
+  Tests at Tahoma
+  Front bumper on the line: 3000 RPM, 55 degrees hood, distance 131 inches
+  */
 }
